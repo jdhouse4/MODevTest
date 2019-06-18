d7a4b528569e195dc41e0066f6690d92b0743d57
Jim Hillhouse <jdhouse4@me.com>
2019-06-17 20:33:15 -0700

    Initial Commit

--- MODevTest/GameViewController.swift
+++ MODevTest/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/cube.scn")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -24,13 +24,16 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        lightNode.light!.type = .directional
+        lightNode.light!.intensity = 1000.0
+        lightNode.light!.categoryBitMask = 2
+        lightNode.light!.castsShadow = true
+        lightNode.position = SCNVector3(x: 0, y: 0, z: 10)
         scene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
@@ -41,10 +44,15 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        let cube = scene.rootNode.childNode(withName: "Cube_Blender", recursively: true)!
+
+        let cubeNode        = SCNNode()
+        cubeNode.geometry   = SCNBox()
+    
+
         
         // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        //cube.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
